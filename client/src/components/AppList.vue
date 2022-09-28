50171c3b1cddfd3bdb478655e0006f2b789674ce
Tuise233 <2039203690@qq.com>
2022-09-28 23:10:55 +0800

    修复已知问题 删除快捷访问功能实现

--- client/src/components/AppList.vue
+++ client/src/components/AppList.vue
@@ -1,112 +1,229 @@
 <template>
   <div class="app-applist">
     <el-carousel indicator-position="outsize" :autoplay="false" arrow="never">
-        <el-carousel-item v-for="(page, pIndex) in $store.state.formatAppList" v-bind:key="pIndex">
-            <div class="app-applist-container">
-                <div class="app-applist-container-item-row" v-for="(row, rIndex) in page" v-bind:key="rIndex">
-                    <div class="app-applist-container-item" v-for="(item, index) in row" v-bind:key="index"
-                        @click="handleOpenApp(item.url)">
-                        <div class="app-applist-container-item-img" :style="{ backgroundColor: item.color == '' ? 'rgba(155, 155, 155, 0.384)' : item.color }">
-                            <img v-if="item.iconType == 0 && item.icon != ''" :src="item.icon">
-                            <span v-if="item.iconType == 1 && item.icon != ''" style="font-weight: bold; font-size: 20px; color:white;">{{ item.name[0] }}</span>
-                        </div>
-                        <span>{{item.name}}</span>
-                    </div>
+      <el-carousel-item
+        v-for="(page, pIndex) in $store.state.formatAppList"
+        v-bind:key="pIndex"
+      >
+        <div class="app-applist-container">
+          <div
+            class="app-applist-container-item-row"
+            v-for="(row, rIndex) in page"
+            v-bind:key="rIndex"
+          >
+            <div
+              class="app-applist-container-item"
+              v-for="(item, index) in row"
+              v-bind:key="index"
+              :class="{ swing: isEdit }"
+            >
+              <div
+                class="app-applist-container-item-img"
+                :style="{
+                  backgroundColor:
+                    item.color == ''
+                      ? 'rgba(155, 155, 155, 0.384)'
+                      : item.color,
+                }"
+                @contextmenu.prevent="editApp()"
+                @click="handleOpenApp(item.url)"
+                style="position: relative"
+              >
+                <div
+                  style="
+                    position: absolute;
+                    width: 20px;
+                    height: 20px;
+                    border-radius: 50%;
+                    background-color: rgba(77, 75, 75, 0.7);
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    top: -5px;
+                    right: -5px;
+                  "
+                  v-if="isEdit == true"
+                >
+                  <i
+                    class="el-icon-close"
+                    style="font-size: 12px; margin-left: 1px"
+                    @click="handleDeleteApp(item)"
+                  ></i>
                 </div>
+                <img
+                  v-if="item.iconType == 0 && item.icon != ''"
+                  :src="item.icon"
+                />
+                <span
+                  v-if="item.iconType == 1 && item.icon != ''"
+                  style="font-weight: bold; font-size: 20px; color: white"
+                  >{{ item.name[0] }}</span
+                >
+              </div>
+              <span>{{ item.name }}</span>
             </div>
-        </el-carousel-item>
+          </div>
+        </div>
+      </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            pageCount: 1, //页数
-        }
+  data() {
+    return {
+      pageCount: 1, //页数
+      isEdit: false,
+    };
+  },
+
+  created() {
+    this.$store.commit("formatList");
+
+    document.addEventListener(
+      "click",
+      () => {
+        this.isEdit = false;
+      },
+      false
+    );
+  },
+
+  methods: {
+    handleOpenApp(url) {
+      if (!this.isEdit) {
+        window.open(url, "_self");
+      }
     },
 
-    created() {
+    editApp() {
+      this.isEdit = true;
+    },
+
+    handleDeleteApp(item) {
+      this.$confirm(`确定要删除${item.name}?`, "提示", {
+        confirmButtonText: "删除",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(() => {
+        this.isEdit = true;
+
+        let apps = this.$store.state.userData.appList;
+        let newApp = [];
+        for(let i = 0; i < apps.length; i++){
+            if(apps[i] != item){
+                newApp.push(apps[i]);
+            }
+        }
+
+        this.$store.state.userData.appList = newApp;
+        this.$store.commit("saveUserData");
         this.$store.commit("formatList");
-    },
 
-    methods: {
-        handleOpenApp(url){
-            window.open(url, "_self");
-        }
+      }).catch(() => {
+        this.isEdit = true;
+      });
     },
-}
+  },
+};
 </script>
 
 <style>
 /* 当客户端为手机时不显示App列表 */
 @media screen and (max-width: 1000px) {
-    .app-appList{
-        display: none;
-    }
+  .app-appList {
+    display: none;
+  }
 }
 
-.app-applist{
-    width: 700px;
-    height: 190px;
+.app-applist {
+  width: 700px;
+  height: 190px;
 }
 
-.app-applist-container{
-    width: 100%;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.app-applist-container {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.app-applist-container-item-row{
-    width: 100%;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    /* justify-content: space-around; */
-    margin: 30px 0;
+.app-applist-container-item-row {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  /* justify-content: space-around; */
+  margin: 30px 0;
 }
 
-.app-applist-container-item{
-    margin-left: 35px;
-    margin-right: 35px;
-    width: 70px;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    transition: 0.3s all;
-    color: white;
+.app-applist-container-item {
+  margin-left: 35px;
+  margin-right: 35px;
+  width: 70px;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s all;
+  color: white;
 }
 
-.app-applist-container-item-img{
-    width: 50px;
-    height: 50px;
-    /* background-color: rgba(155, 155, 155, 0.384); */
-    /* background-color: rgba(255, 255, 255, 0.95); */
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 5px;
-    transition: 0.3s all;
-    box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
+.app-applist-container-item-img {
+  width: 50px;
+  height: 50px;
+  /* background-color: rgba(155, 155, 155, 0.384); */
+  /* background-color: rgba(255, 255, 255, 0.95); */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  transition: 0.3s all;
+  box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
 }
 
-.app-applist-container-item-img:hover{
-    cursor: pointer;
-    background-color: white;
+.app-applist-container-item-img:hover {
+  cursor: pointer;
+  background-color: white;
 }
 
-.app-applist-container-item span{
-    font-size: 13px;
-    margin-top: 5px;
+.app-applist-container-item span {
+  font-size: 13px;
+  margin-top: 5px;
 }
 
-.app-applist-container-item img{
-    width: 30px;
-    height: 30px;
+.app-applist-container-item img {
+  width: 30px;
+  height: 30px;
+}
+
+.swing {
+  animation: aswing ease 0.3s infinite;
+  transform-origin: center 50px;
+}
+
+@keyframes aswing {
+  0% {
+    transform: rotate(0) translateX(-0.5px) scale(1);
+  }
+
+  20% {
+    transform: rotate(-1deg) translateY(-0.5px) scale(1);
+  }
+
+  60% {
+    transform: rotate(0) scale(1);
+  }
+
+  80% {
+    transform: rotate(1deg) translateX(0.5px) scale(1);
+  }
+
+  100% {
+    transform: rotate(0) translateY(0.5px) scale(1);
+  }
 }
 </style>
--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,26 +1,44 @@
 <template>
-  <div @contextmenu="disableMenu" class="app-background" :style="{ backgroundImage: `url('${backgroundUrl}')` }">
-		<meta name="viewport" content="viewport-fit=cover">
-		<div class="app-container" :style="{ backdropFilter: `blur(${$store.state.userData.backBlur}px)` }">
-			<!-- 导航栏 -->
-			<NavBar />
+  <div
+    @contextmenu="disableMenu"
+    class="app-background"
+    :style="{
+      backgroundImage: backgroundType == 1 ? '' : `url('${backgroundUrl}')`,
+    }"
+  >
+  <div class="mask" style="width: 100%; height: 100%; position: absolute; background-color: rgb(56, 56, 56); transition: 0.8s all;"></div>
+    <video
+      v-if="backgroundType == 1"
+      class="app-bg-video"
+      autoplay="true"
+      loop="true"
+      muted="true"
+      :src="backgroundUrl"
+    ></video>
+    <meta name="viewport" content="viewport-fit=cover" />
+    <div
+      class="app-container"
+      :style="{ backdropFilter: `blur(${$store.state.userData.backBlur}px)` }"
+    >
+      <!-- 导航栏 -->
+      <NavBar />
 
-			<!-- 时间 -->
-			<DateTime />
+      <!-- 时间 -->
+      <DateTime />
 
-			<!-- 搜索栏 -->
-			<SearchBox />
+      <!-- 搜索栏 -->
+      <SearchBox />
 
-			<!-- 选项侧边栏 -->
-			<Setting />
+      <!-- 选项侧边栏 -->
+      <Setting />
 
-			<!-- 应用列表 -->
-			<AppList />
+      <!-- 应用列表 -->
+      <AppList />
 
-			<!-- 一言 -->
-			<OneWord />
-		</div>
-	</div>
+      <!-- 一言 -->
+      <OneWord />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -34,73 +52,95 @@ import OneWord from "@/components/OneWord.vue";
 
 export default {
   components: {
-		NavBar,
-		DateTime,
-		SearchBox,
-		Setting,
-		AppList,
-		OneWord
+    NavBar,
+    DateTime,
+    SearchBox,
+    Setting,
+    AppList,
+    OneWord,
   },
 
   created() {
-	document.title = "拉布拉页"
-	this.backgroundUrl = this.backgrounds[Math.floor(Math.random() * this.backgrounds.length)];
+    document.title = "拉布拉页";
+    this.backgroundUrl =
+      this.backgrounds[Math.floor(Math.random() * this.backgrounds.length)];
+
+    if (this.backgroundUrl.includes(".mp4")) {
+      this.backgroundType = 1;
+    }
+
+	setTimeout(() => {
+		let mask = document.getElementsByClassName("mask")[0];
+		mask.style.opacity = "0";
+	}, 300);
   },
 
   data() {
-	return {
-		backgrounds:[
-			"https://images.pexels.com/photos/9921401/pexels-photo-9921401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            "https://images.pexels.com/photos/9941707/pexels-photo-9941707.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-            "https://images.pexels.com/photos/9921341/pexels-photo-9921341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            "https://images.pexels.com/photos/11739438/pexels-photo-11739438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            "https://images.pexels.com/photos/9921356/pexels-photo-9921356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            "https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-		],
+    return {
+      backgrounds: [
+		"https://images.pexels.com/photos/9921401/pexels-photo-9921401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/9941707/pexels-photo-9941707.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        "https://images.pexels.com/photos/9921341/pexels-photo-9921341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/11739438/pexels-photo-11739438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/9921356/pexels-photo-9921356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://az3.sfo2.cdn.digitaloceanspaces.com/bingdesktop/3455b6778764415593fb5e688218fb89.mp4",
+      ],
 
-		backgroundUrl: ""
-	}
+      backgroundUrl: "",
+      backgroundType: 0, //0 - Image | 1 - Video
+    };
   },
 
   methods: {
-	disableMenu(){
-		window.event.returnValue = false;
-      	return false;
-	}
+    disableMenu() {
+      window.event.returnValue = false;
+      return false;
+    },
   },
 };
 </script>
 
 <style>
-html, body{
-	margin: 0;
-	padding: 0;
+html,
+body {
+  margin: 0;
+  padding: 0;
 }
 
 @font-face {
-	font-family: "PingFang";
-	src: url("@/assets/font/pingfang.ttf");
+  font-family: "PingFang";
+  src: url("@/assets/font/pingfang.ttf");
 }
 
-*{
-	font-family: "PingFang";
-	user-select: none;
+* {
+  font-family: "PingFang";
+  user-select: none;
 }
 
 .app-background {
-	width: 100vw;
-	height: 100vh;
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
-.app-container{
-	width: 100vw;
-	height: 100vh;
-  	/* backdrop-filter: blur(10px); */
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+.app-bg-video {
+  position: absolute;
+  display: block;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  transition: 0.3s;
+}
+
+.app-container {
+  width: 100vw;
+  height: 100vh;
+  /* backdrop-filter: blur(10px); */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
